606a3ab6018b90fb45c12827756b0d73f0f0bb07
saliya rivinath <128215926+alphasaliya@users.noreply.github.com>
2024-02-11 15:00:44 +0530

    Add files via upload
    libraries

---
+++ EnnerfyEffixML.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from pandas.compat import pyarrow as pd
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+
+##energy Effis SDCG 109
+
