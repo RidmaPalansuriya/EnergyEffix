ee655e227d8dd4845b5db6646c6c56c0e418724c
saliya rivinath <128215926+alphasaliya@users.noreply.github.com>
2024-03-24 14:04:01 +0530

    Update customappbar_test.dart

--- energyeffix_app/test/customappbar_test.dart
+++ energyeffix_app/test/customappbar_test.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:energyeffix_app/custom_app_bar.dart';
+import 'package:energyeffix_app/Components/CustomAppBar.dart';
 
 void main() {
-  testWidgets('CustomAppBar renders correctly', (WidgetTester tester) async {
+  testWidgets('CustomAppBar widget test', (WidgetTester tester) async {
+    // Build the widget
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          appBar: CustomAppBar(
+            pageTitle: 'Test Page',
+            onBackPress: () {}, // Provide a dummy callback for testing
+          ),
+        ),
+      ),
+    );
 
-   
+    // Verify that the page title is displayed
+    expect(find.text('Test Page'), findsOneWidget);
 
-    // Verify that the menu icon is present
-    expect(find.byIcon(Icons.menu), findsOneWidget);
+    // Verify that the back button works
+    await tester.tap(find.byIcon(Icons.arrow_back_ios_new_rounded));
+    await tester.pump(); // Rebuild the widget after tapping
+    // Add expectations based on the expected behavior of the back button
 
-    // Verify that the notification icon is present
-    expect(find.byIcon(Icons.add_alert), findsOneWidget);
-
-    // Verify that the profile icon is present
-    expect(find.byIcon(Icons.account_circle), findsOneWidget);
-
-    // Verify that the title text is present
-    expect(find.text('Test Title'), findsOneWidget);
-
-    // You can add more specific tests for other widgets and properties if needed
+    // Verify that the leading icon button opens the drawer when onBackPress is not provided
+    await tester.tap(find.byIcon(Icons.menu));
+    await tester.pump(); // Rebuild the widget after tapping
+    // Add expectations based on the expected behavior of the leading icon button
   });
 }
